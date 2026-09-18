b7f431b18c89b03addb866ef558cd604eb9a73cd
FilipiSantoz <239681201+FilipiSantoz@users.noreply.github.com>
2026-09-18 16:49:21 -0300

    Add some custom highlights

--- config/colorschemes/catppuccin.nix
+++ config/colorschemes/catppuccin.nix
@@ -21,9 +21,9 @@
           integrations = {
             cmp = true;
             noice = true;
-            notify = true;
+            notify = false;
             neotree = true;
-            harpoon = true;
+            harpoon = false;
             gitsigns = true;
             which_key = true;
             illuminate = {
@@ -47,6 +47,35 @@
               };
             };
           };
+          custom_highlights.__raw = ''
+            function(colors)
+              return {
+                LineNr = { fg = "#ffffff" },
+                Comment = { fg = "#85858f" },
+                Boolean = { fg = "#e0a760" },
+                Function = { fg = "#ceafff" },
+                Conditional = { fg = "#5f5fc2" },
+                IncSearch = { fg = "#000000", bg = "#b1f2dc" },
+                CursorLineNr = { fg = "#ebd3f8" },
+                BufferLineSeparator = { fg = "#000000" },
+                NeoTreeModified = { fg = "#1fdedc" },
+                NeoTreeGitModified = { fg = "#34d5e3" },
+                NeoTreeRootName = { fg = "#ffffff" },
+                NeoTreeDirectoryIcon = { fg = "#ceafff" },
+                NeoTreeDirectoryName = { fg = "#dbdbdb" },
+                NeoTreeIndentMarker = { fg = "#aaaaaa" },
+                NeoTreeTabActive = { fg = "#ffffff" },
+                NeoTreeTabSeparatorInactive = { bg = "NONE", fg = "#000000" },
+                NeoTreeTabSeparatorActive = { bg = "NONE", fg = "#ffffff" },
+                NormalFloat = { bg = "NONE" },
+                FloatTitle = { bg = "NONE", fg = "#ffffff" },
+                FloatBorder = { bg = "NONE", fg = "#87f6fd" },
+                SnacksPickerBorder = { bg = "None", fg = "#87f6fd" },
+                Directory = { fg = "#ceafff" },
+
+              }
+            end
+          '';
         };
       };
     };
